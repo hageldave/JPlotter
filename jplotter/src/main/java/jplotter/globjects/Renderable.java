762407b1ddebc1dd22726b892f678c9819c8eb46
David <david@RECHENKNECHTLNX>
2019-05-17 00:58:31 +0200

    documentation for Renderable

--- jplotter/src/main/java/jplotter/globjects/Renderable.java
+++ jplotter/src/main/java/jplotter/globjects/Renderable.java
@@ -1,15 +1,46 @@
 package jplotter.globjects;
 
-public interface Renderable extends AutoCloseable {
+import jplotter.Annotations.GLContextRequired;
+import jplotter.renderers.GenericRenderer;
+import jplotter.renderers.Renderer;
 
-	public void close();
+/**
+ * Interface for an object that can be rendered by a {@link Renderer} e.g. the {@link GenericRenderer}.
+ * It is intended for objects that contain openGL resources such as a {@link VertexArray} for example.
+ * For this reason the interface extends the {@link AutoCloseable} interface for disposing of GL resources
+ * on {@link #close()}.
+ * Its state can be dirty ({@link #isDirty()}) which means that the GL resources are not in sync with the object and implies that
+ * a call to the {@link #updateGL()} method is necessary before rendering.
+ * The {@link #initGL()} method has to be called once before the first rendering to allocate required GL resources.
+ * 
+ * @author hageldave
+ */
+public interface Renderable extends AutoCloseable {
 	
-	/** should return early if already initialized */
+	/** 
+	 * Allocates GL resources for this Renderable such as a {@link VertexArray}.
+	 * Should return early if already initialized.
+	 */
+	@GLContextRequired
 	public void initGL();
 	
+	/**
+	 * if true, indicates that a call to {@link #updateGL()} is necessary to sync this objects GL resources
+	 * to its current state.
+	 * @return true if dirty
+	 */
+	public boolean isDirty();
+	
+	/**
+	 * updates GL resources to match this objects state.
+	 */
+	@GLContextRequired
 	public void updateGL();
 	
-	/** indicates that {@link #updateGL()} has to be called */
-	public boolean isDirty();
+	/**
+	 * disposes of any GL resources belonging to this object.
+	 */
+	@GLContextRequired
+	public void close();
 	
 }
