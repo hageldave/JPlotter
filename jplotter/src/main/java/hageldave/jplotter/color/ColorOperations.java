b23abb87edf1533b5b562871d4a77ac80b556985
haegeldd <haegele.david@gmail.com>
2019-08-05 17:50:59 +0200

    coloroperations class for convenient manipulation of int packed ARGB

---
+++ jplotter/src/main/java/hageldave/jplotter/color/ColorOperations.java
@@ -0,0 +1,15 @@
+package hageldave.jplotter.color;
+
+import static hageldave.imagingkit.core.Pixel.*;
+
+public class ColorOperations {
+
+	public static int mixARGB(int argb1, int argb2){
+		return argb(
+				(a(argb1)+a(argb2))/2, 
+				(r(argb1)+r(argb2))/2, 
+				(g(argb1)+g(argb2))/2, 
+				(b(argb1)+b(argb2))/2);
+	}
+	
+}
