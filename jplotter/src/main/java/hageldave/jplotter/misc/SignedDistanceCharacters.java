ac0ed842e68fdf81b2f4906194ac9f0c390529bb
David <david@THINKPADLNX>
2019-08-08 16:04:41 +0200

    SignedDistanceCharacters now as object with its characters image and corresponding bounds for the characters

--- jplotter/src/main/java/hageldave/jplotter/misc/SignedDistanceCharacters.java
+++ jplotter/src/main/java/hageldave/jplotter/misc/SignedDistanceCharacters.java
@@ -1,32 +1,81 @@
 package hageldave.jplotter.misc;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
+import java.awt.Rectangle;
 
 import hageldave.imagingkit.core.Img;
 import hageldave.imagingkit.core.Pixel;
 import hageldave.imagingkit.core.util.ImageFrame;
+import hageldave.jplotter.util.Pair;
 
 public class SignedDistanceCharacters {
 	
-	public static final int genFontSize = 32;
-	public static final int padding = 8;
-	
+	protected static final int genFontSize = 32;
+	protected static final int padding = 8;
 	protected static final Img FONTMETRIC_IMG = new Img(64, 64);
+	public static final String CHARACTERS = 
+			" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 	
-	public static Img mkCharDistanceField(char ch, String fontname, int style) {
-		Font f = new Font(fontname, style, genFontSize);
+	
+	protected int[] leftBounds = new int[CHARACTERS.length()];
+	protected int[] rightBounds = new int[CHARACTERS.length()];
+	protected int[] topBounds = new int[CHARACTERS.length()];
+	protected int[] botBounds = new int[CHARACTERS.length()];
+	protected final Img texImg;
+	protected final String fontname;
+	protected final int style;
+	protected final Font font;
+	
+	public SignedDistanceCharacters(String fontname, int style) {
+		this.fontname = fontname;
+		this.style = style;
+		this.font = new Font(fontname, style, genFontSize);
+		// create texture and setup character bounds
+		Dimension dim = mkCharDistanceField(' ', font).first.getDimension();
+		int charsPerLine = 16;
+		texImg = new Img(dim.width*charsPerLine, dim.height*(CHARACTERS.length()/charsPerLine+1));
+		int idx = 0;
+		for(int j=0; idx<CHARACTERS.length(); j++){
+			for(int i=0; i<charsPerLine && idx<CHARACTERS.length(); i++){
+				// make texture for character
+				Pair<Img, Rectangle> next = mkCharDistanceField(CHARACTERS.charAt(idx), font);
+				// put character into collection image
+				int x = i*dim.width;
+				int y = j*dim.height;
+				next.first.copyArea(0, 0, dim.width, dim.height, texImg, x, y);
+				// set bounds for character
+				leftBounds[idx]  = x+(int)next.second.getMinX();
+				rightBounds[idx] = x+(int)next.second.getMaxX();
+				topBounds[idx] = y+(int)next.second.getMinY();
+				botBounds[idx] = y+(int)next.second.getMaxY();
+//				int k = idx;
+//				texImg.paint(g->{
+//					g.setColor(Color.CYAN);
+//					g.drawRect(leftBounds[k], topBounds[k], rightBounds[k]-leftBounds[k], botBounds[k]-topBounds[k]);
+//				});
+				idx++;
+			}
+		}
+	}
+	
+	protected static Pair<Img,Rectangle> mkCharDistanceField(char ch, Font f) {
 		// determine width of character and descent for font
 		int[] advance = {0};
 		int[] descent = {0};
 		int[] fontHeight = {0};
+		Rectangle bounds = new Rectangle();
 		FONTMETRIC_IMG.paint(g2d->{
 			FontMetrics fontMetrics = g2d.getFontMetrics(f);
 			descent[0] = fontMetrics.getDescent();
 			advance[0] = fontMetrics.getMaxAdvance();
 			fontHeight[0] = fontMetrics.getHeight();
+			Rectangle b = fontMetrics.getStringBounds(""+ch, g2d).getBounds();
+			bounds.setRect(b);
 		});
+		bounds.setLocation(padding, padding);
 		// create signed distance field image
 		Img tex = new Img(padding*2+advance[0], padding*2+fontHeight[0]).fill(0xff000000);
 		tex.paint(g2d->{
@@ -49,7 +98,15 @@ public class SignedDistanceCharacters {
 		tex.forEach(px->{
 			px.setRGB(px.g(), px.g(), px.g());
 		});
-		return tex;
+		// DEBUG
+		tex.paint(g2d->{
+			g2d.setColor(Color.blue);
+			g2d.setFont(f);
+			g2d.drawString(""+ch, padding, padding+fontHeight[0]-descent[0]);
+			g2d.setColor(Color.GREEN);
+			g2d.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+		});
+		return Pair.of(tex, bounds);
 	}
 	
 	static double edgeDistance(Pixel px, int maxDist){
@@ -71,19 +128,8 @@ public class SignedDistanceCharacters {
 	}
 	
 	public static void main(String[] args) {
-		String characters = CharacterAtlas.CHARACTERS;
-		Img img = mkCharDistanceField(characters.charAt(0), CharacterAtlas.FONT_NAME, Font.PLAIN);
-		System.out.println(img.getWidth());
-		for(int i=1; i < characters.length(); i++){
-			Img next = mkCharDistanceField(characters.charAt(i), CharacterAtlas.FONT_NAME, Font.PLAIN);
-			Img merge = new Img(img.getWidth()+next.getWidth(), img.getHeight());
-			img.copyArea(0, 0, img.getWidth(), img.getHeight(), merge, 0, 0);
-			next.copyArea(0, 0, next.getWidth(), next.getHeight(), merge, img.getWidth(), 0);
-			img = merge;
-		}
-		ImageFrame.display(img);
-		System.out.println(img.getWidth());
-		System.out.println(characters.length());
+		SignedDistanceCharacters signedDistanceCharacters = new SignedDistanceCharacters(CharacterAtlas.FONT_NAME, Font.PLAIN);
+		ImageFrame.display(signedDistanceCharacters.texImg);
 	}
 	
 }
