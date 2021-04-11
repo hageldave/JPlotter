749753fe8cdfe12627be3caf2f6c32e8eb84b3e3
Luca Reichmann <luca@lucareichmann.de>
2021-04-12 00:34:06 +0200

    added a zoom functionality by selecting a rectangle and added a getter method for the pointMap

--- jplotter/src/main/java/hageldave/jplotter/charts/ScatterPlot.java
+++ jplotter/src/main/java/hageldave/jplotter/charts/ScatterPlot.java
@@ -5,6 +5,7 @@ import hageldave.jplotter.canvas.BlankCanvasFallback;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 import hageldave.jplotter.interaction.CoordSysPanning;
 import hageldave.jplotter.interaction.CoordSysScrollZoom;
+import hageldave.jplotter.interaction.CoordSysViewSelector;
 import hageldave.jplotter.misc.DefaultGlyph;
 import hageldave.jplotter.renderables.Points;
 import hageldave.jplotter.renderers.CoordSysRenderer;
@@ -20,10 +21,10 @@ import java.util.Map;
  * which are all set up automatically.
  * To edit those, they can be returned with their respective getter methods.
  * When data points are added to the ScatterPlot, they are stored in the pointMap in a {@link Dataset}.
- *
+ * <p>
  * To add a Dataset to the pointMap, an ID has to be defined as a key.
  * With this ID the Dataset can be removed later on.
- *
+ * <p>
  * In each {@link Dataset} there are the coordinates of the points,
  * the selected glyph & color and the {@link Points} stored.
  *
@@ -45,7 +46,7 @@ public class ScatterPlot {
         protected Color color;
         protected Points points;
 
-        protected Dataset(final double[][] pointsCoordinates, final DefaultGlyph glyph, final Color color, final Points points) {
+        protected Dataset (final double[][] pointsCoordinates, final DefaultGlyph glyph, final Color color, final Points points) {
             this.pointsCoordinates = pointsCoordinates;
             this.glyph = glyph;
             this.color = color;
@@ -69,13 +70,13 @@ public class ScatterPlot {
         }
     }
 
-    public ScatterPlot(final boolean useOpenGL) {
+    public ScatterPlot (final boolean useOpenGL) {
         this.pointMap = new HashMap<Integer, Dataset>();
         this.canvas = useOpenGL ? new BlankCanvas() : new BlankCanvasFallback();
         setupScatterPlot();
     }
 
-    public ScatterPlot(final JPlotterCanvas canvas) {
+    public ScatterPlot (final JPlotterCanvas canvas) {
         this.pointMap = new HashMap<Integer, Dataset>();
         this.canvas = canvas;
         setupScatterPlot();
@@ -84,12 +85,12 @@ public class ScatterPlot {
     /**
      * Helper method to set the initial scatter plot.
      */
-    protected void setupScatterPlot() {
+    protected void setupScatterPlot () {
         this.canvas.asComponent().setPreferredSize(new Dimension(400, 400));
         this.canvas.asComponent().setBackground(Color.WHITE);
         this.coordsys = new CoordSysRenderer();
         this.content = new PointsRenderer();
-        this.coordsys.setCoordinateView(-1,-1,1,1);
+        this.coordsys.setCoordinateView(-1, -1, 1, 1);
         this.coordsys.setContent(content);
         this.canvas.setRenderer(coordsys);
     }
@@ -97,16 +98,16 @@ public class ScatterPlot {
     /**
      * adds a set of points to the scatter plot.
      *
-     * @param ID the ID is the key with which the Dataset will be stored in the pointMap
+     * @param ID     the ID is the key with which the Dataset will be stored in the pointMap
      * @param points a double array containing the coordinates of the points
-     * @param glyph the data points will be visualized by that glyph
-     * @param color the color of the glyph
+     * @param glyph  the data points will be visualized by that glyph
+     * @param color  the color of the glyph
      * @return the old Scatterplot for chaining
      */
-    public ScatterPlot addPoints(final int ID, final double[][] points, final DefaultGlyph glyph, final Color color) {
+    public ScatterPlot addPoints (final int ID, final double[][] points, final DefaultGlyph glyph, final Color color) {
         ScatterPlot old = this;
         Points tempPoints = new Points(glyph);
-        for(double[] entry: points) {
+        for (double[] entry : points) {
             double x = entry[0], y = entry[1];
             Points.PointDetails point = tempPoints.addPoint(x, y);
             point.setColor(color);
@@ -123,7 +124,7 @@ public class ScatterPlot {
      * @param ID the Dataset with this ID will be removed from the pointMap
      * @return the old Scatterplot for chaining
      */
-    public ScatterPlot removePoints(final int ID) {
+    public ScatterPlot removePoints (final int ID) {
         ScatterPlot old = this;
         this.pointMap.remove(ID);
         return old;
@@ -132,22 +133,37 @@ public class ScatterPlot {
     /**
      * Adds a scroll zoom to the Scatterplot
      *
-     * @return the CoordSysScrollZoom so that it can be further customized
+     * @return the {@link CoordSysScrollZoom} so that it can be further customized
      */
-    public CoordSysScrollZoom addScrollZoom() {
+    public CoordSysScrollZoom addScrollZoom () {
         return new CoordSysScrollZoom(this.canvas, this.coordsys).register();
     }
 
     /**
      * Adds panning functionality to the Scatterplot
      *
-     * @return the CoordSysPanning so that it can be further customized
+     * @return the {@link CoordSysPanning} so that it can be further customized
      */
-    public CoordSysPanning addPanning() {
+    public CoordSysPanning addPanning () {
         return new CoordSysPanning(this.canvas, this.coordsys).register();
     }
 
-    public ScatterPlot useOpenGL(final boolean useOpenGL) {
+    /**
+     * Adds a zoom functionality by selecting a rectangle.
+     *
+     * @return the {@link CoordSysViewSelector} so that it can be further customized
+     */
+    public CoordSysViewSelector addZoomViewSelector () {
+        return new CoordSysViewSelector(this.canvas, this.coordsys) {
+            { extModifierMask = 0;/* no need for shift to be pressed */ }
+            @Override
+            public void areaSelected (double minX, double minY, double maxX, double maxY) {
+                coordsys.setCoordinateView(minX, minY, maxX, maxY);
+            }
+        }.register();
+    }
+
+    public ScatterPlot useOpenGL (final boolean useOpenGL) {
         this.canvas = useOpenGL ? new BlankCanvas() : new BlankCanvasFallback();
         setupScatterPlot();
         restoreDatapoints();
@@ -158,10 +174,10 @@ public class ScatterPlot {
      * Helper method which restores the data points from the pointMap
      * when the canvas is 'reloaded'
      */
-    protected void restoreDatapoints() {
-        for (Map.Entry<Integer, Dataset> dataset: this.pointMap.entrySet()) {
+    protected void restoreDatapoints () {
+        for (Map.Entry<Integer, Dataset> dataset : this.pointMap.entrySet()) {
             Points tempPoints = new Points(dataset.getValue().getGlyph());
-            for(double[] entry: dataset.getValue().getPointsCoordinates()) {
+            for (double[] entry : dataset.getValue().getPointsCoordinates()) {
                 double x = entry[0], y = entry[1];
                 Points.PointDetails point = tempPoints.addPoint(x, y);
                 point.setColor(dataset.getValue().getColor());
@@ -180,5 +196,9 @@ public class ScatterPlot {
 
     public PointsRenderer getContent () {
         return content;
+    }
+
+    public HashMap<Integer, Dataset> getPointMap () {
+        return pointMap;
     }
 }
