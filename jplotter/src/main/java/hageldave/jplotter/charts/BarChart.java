a1cf355a629cdc78e8c5a62df750784530d843f8
Luca Reichmann <luca@lucareichmann.de>
2021-05-29 20:25:54 +0200

    continued work on barchart

--- jplotter/src/main/java/hageldave/jplotter/charts/BarChart.java
+++ jplotter/src/main/java/hageldave/jplotter/charts/BarChart.java
@@ -5,65 +5,69 @@ import hageldave.jplotter.canvas.BlankCanvasFallback;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 import hageldave.jplotter.coordsys.TickMarkGenerator;
 import hageldave.jplotter.renderables.Triangles;
+import hageldave.jplotter.renderers.BarRenderer;
 import hageldave.jplotter.renderers.CompleteRenderer;
-import hageldave.jplotter.renderers.CoordSysRenderer;
+import hageldave.jplotter.util.AlignmentConstants;
 import hageldave.jplotter.util.Pair;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 
-// TODO currently Triangles are used to display the rectangles - this creates overhead (e.g. when changing color of a bar)
 public class BarChart {
+    final public TreeMap<Integer, BarStruct> trianglesInRenderer = new TreeMap<>();
+    final protected double barSize = 0.8;
     protected CompleteRenderer content;
     protected JPlotterCanvas canvas;
-    protected CoordSysRenderer coordsys;
-    protected Alignment chartAlignment;
-    final protected double barSize = 0.8;
-    protected int barCount;
-    final public HashMap<Integer, BarStruct> trianglesInRenderer = new HashMap<>();
-    final protected ArrayList<double[]> dataAdded = new ArrayList<>();
-
-    public enum Alignment {
-        VERTICAL,
-        HORIZONTAL
-    }
+    protected BarRenderer barRenderer;
+    protected int chartAlignment;
+    protected int barCount = 0;
 
     public static class BarStruct {
-        public Triangles tri;
+        final public LinkedList<Triangles> stacks = new LinkedList<>();
+        final public LinkedList<Double> dataSets = new LinkedList<>();
         public String descr;
+        public int position;
+        public double barLength;
 
-        public BarStruct(final Triangles tri, final String descr) {
-            this.tri = tri;
+        public BarStruct(final Triangles stacks, final int position, final String descr, final double dataSet) {
+            this.stacks.add(stacks);
             this.descr = descr;
+            this.position = position;
+            this.dataSets.add(dataSet);
+        }
+
+        public BarStruct(final Triangles stacks, final int position, final String descr, final double dataSet, final double barLength) {
+            this.stacks.add(stacks);
+            this.descr = descr;
+            this.position = position;
+            this.barLength = barLength;
+            this.dataSets.add(dataSet);
         }
     }
 
     public BarChart(final boolean useOpenGL) {
-        this(useOpenGL ? new BlankCanvas() : new BlankCanvasFallback(), Alignment.HORIZONTAL, "X", "Y");
+        this(useOpenGL ? new BlankCanvas() : new BlankCanvasFallback(), AlignmentConstants.HORIZONTAL, "X", "Y");
     }
 
-    public BarChart(final boolean useOpenGL, final Alignment chartAlignment) {
+    public BarChart(final boolean useOpenGL, final int chartAlignment) {
         this(useOpenGL ? new BlankCanvas() : new BlankCanvasFallback(), chartAlignment, "X", "Y");
     }
 
-    public BarChart(final JPlotterCanvas canvas, final Alignment chartAlignment, final String xLabel, final String yLabel) {
+    public BarChart(final JPlotterCanvas canvas, final int chartAlignment, final String xLabel, final String yLabel) {
         this.canvas = canvas;
         this.canvas.asComponent().setPreferredSize(new Dimension(400, 400));
         this.canvas.asComponent().setBackground(Color.WHITE);
-        this.coordsys = new CoordSysRenderer();
+        this.barRenderer = new BarRenderer(chartAlignment);
         this.content = new CompleteRenderer();
-        this.coordsys.setCoordinateView(-1, -1, 1, 1);
-        this.coordsys.setContent(content);
-        this.canvas.setRenderer(coordsys);
-        this.coordsys.setxAxisLabel(xLabel);
-        this.coordsys.setyAxisLabel(yLabel);
+        this.barRenderer.setCoordinateView(-1, -1, 1, 1);
+        this.barRenderer.setContent(content);
+        this.canvas.setRenderer(barRenderer);
+        this.barRenderer.setxAxisLabel(xLabel);
+        this.barRenderer.setyAxisLabel(yLabel);
         this.chartAlignment = chartAlignment;
     }
 
@@ -76,23 +80,30 @@ public class BarChart {
         return addData(new int[]{ID}, new double[]{val}, new Color[]{color}, new String[]{descr});
     }
 
-    // TODO ensure all arrays have equal size
+    public BarChart addBar(final int ID, final double val, final Color color) {
+        return addData(new int[]{ID}, new double[]{val}, new Color[]{color}, new String[]{""});
+    }
+
     public BarChart addData(final int[] IDs, final double[] data, final Color[] color, final String[] descr) {
+        if (!(IDs.length == data.length && data.length == color.length && color.length == descr.length))
+            throw new IllegalArgumentException("All arrays have to have equal size!");
         for (int i = 0; i < data.length; i++) {
-            Triangles triangleRend = makeBar(i, data[i], color[i]);
-            this.trianglesInRenderer.put(IDs[i], new BarStruct(triangleRend, descr[i]));
-            this.content.addItemToRender(triangleRend);
+            if (this.trianglesInRenderer.containsKey(IDs[i])) {
+                addStack(IDs[i], data[i], color[i]);
+            } else {
+                Triangles triangleRend = makeBar(barCount++, data[i], color[i]);
+                this.trianglesInRenderer.put(IDs[i],
+                            new BarStruct(triangleRend, barCount, descr[i], data[i], data[i]));
+                this.content.addItemToRender(triangleRend);
+            }
         }
-        dataAdded.add(data);
-        setTickmarks();
-        setBarContentBoundaries();
         return this;
     }
 
     // [i][0: val; 1: index] - oder umgekehrt, needs testing
     public BarChart addData(final int[] IDs, final double[][] data, final Color[] color, final String[] descr) {
         // recalc data
-        HashMap<Double, Double> vals = new HashMap();
+        HashMap<Double, Double> vals = new HashMap<>();
         for (int i = 0; i < data.length; i++) {
             if (!vals.containsKey(data[i][1])) {
                 vals.put(data[i][1], data[i][0]);
@@ -105,116 +116,140 @@ public class BarChart {
         int j = 0;
         for (Double val: vals.values()) {
             Triangles triangleRend = makeBar(j, val, color[j]);
-            this.trianglesInRenderer.put(IDs[j], new BarStruct(triangleRend, descr[j]));
+            //this.trianglesInRenderer.put(IDs[j], new BarStruct(triangleRend, j, descr[j]));
             this.content.addItemToRender(triangleRend);
             j++;
         }
-        // dataAdded.add(data);
-        setTickmarks();
-        setBarContentBoundaries();
         return this;
     }
 
+    // stack will be added on already existing stack
+    protected BarChart addStack(final Integer ID, final double data, final Color color) {
+        Triangles newStack = makeBar(this.trianglesInRenderer.get(ID).barLength,
+                this.trianglesInRenderer.get(ID).position - 1, data, color);
+        this.trianglesInRenderer.get(ID).stacks.add(newStack);
+        this.trianglesInRenderer.get(ID).dataSets.add(data);
+        this.content.addItemToRender(newStack);
+        this.trianglesInRenderer.get(ID).barLength += data;
+        return this;
+    }
 
-    // if new gets added, all the old stuff gets overwritten - fixed
-    protected TickMarkGenerator setTickmarks() {
-        String[] descr = new String[trianglesInRenderer.size()];
-        int j = 0;
-        for (BarStruct val: trianglesInRenderer.values()) {
-            descr[j++] = val.descr;
+    public TickMarkGenerator setTickmarks() {
+        String[] description = new String[trianglesInRenderer.size()];
+        for (int key: trianglesInRenderer.keySet()) {
+            int pos = trianglesInRenderer.get(key).position - 1;
+            description[pos] = trianglesInRenderer.get(key).descr;
         }
-        TickMarkGenerator oldTickGen = coordsys.getTickMarkGenerator();
-        if (this.chartAlignment == Alignment.HORIZONTAL) {
-            coordsys.setTickMarkGenerator((min, max, desired, vert) -> {
-                if (!vert)
-                    return oldTickGen.genTicksAndLabels(min, max, desired, vert);
-                double[] ticks = IntStream.range(0, descr.length).mapToDouble(i -> (double) i).toArray();
-                return Pair.of(ticks, descr);
-            });
-        } else {
-            coordsys.setTickMarkGenerator((min, max, desired, vert) -> {
-                if (vert)
-                    return oldTickGen.genTicksAndLabels(min, max, desired, vert);
-                double[] ticks = IntStream.range(0, descr.length).mapToDouble(i -> (double) i).toArray();
-                return Pair.of(ticks, descr);
-            });
-        }
-        return coordsys.getTickMarkGenerator();
+
+        TickMarkGenerator oldTickGen = barRenderer.getTickMarkGenerator();
+        barRenderer.setTickMarkGenerator((min, max, desired, vert) -> {
+            if (!vert && this.chartAlignment == AlignmentConstants.HORIZONTAL)
+                return oldTickGen.genTicksAndLabels(min, max, desired, vert);
+            if (vert && this.chartAlignment == AlignmentConstants.VERTICAL)
+                return oldTickGen.genTicksAndLabels(min, max, desired, vert);
+            double[] ticks = IntStream.range(0, description.length).mapToDouble(i -> (double) i).toArray();
+            return Pair.of(ticks, description);
+        });
+        return barRenderer.getTickMarkGenerator();
     }
 
     protected Triangles makeBar(final int row, final double val, final Color color) {
+        return makeBar(0, row, val, color);
+    }
+
+    protected Triangles makeBar(final double start, final int row, final double val, final Color color) {
         Triangles bar = new Triangles();
-        if (this.chartAlignment == Alignment.HORIZONTAL) {
-            bar.addQuad(new Rectangle2D.Double(0, row-(barSize/2), val, barSize));
+        if (this.chartAlignment == AlignmentConstants.HORIZONTAL) {
+            bar.addQuad(new Rectangle2D.Double(start, row-(barSize/2), val, barSize));
         } else {
-            bar.addQuad(new Rectangle2D.Double(row-(barSize/2), 0, barSize, val));
+            bar.addQuad(new Rectangle2D.Double(row-(barSize/2), start, barSize, val));
         }
         bar.getTriangleDetails().forEach(tri -> tri.setColor(color));
-        barCount++;
         return bar;
     }
 
-    protected void setBarContentBoundaries() {
-        int maxVal = Integer.MIN_VALUE; int minVal = Integer.MAX_VALUE;
-        if (this.chartAlignment == Alignment.HORIZONTAL) {
-            for (BarStruct value: trianglesInRenderer.values()) {
-                if (value.tri.getBounds().getMaxX() > maxVal)
-                    maxVal = (int) Math.ceil(value.tri.getBounds().getMaxX());
-                if (value.tri.getBounds().getMinX() < minVal)
-                    minVal = (int) Math.ceil(value.tri.getBounds().getMinX());
-            }
-            if (minVal < 0) {
-                this.coordsys.setCoordinateView(minVal - 0.5, -0.8, maxVal + 0.5, barCount);
-            } else {
-                this.coordsys.setCoordinateView(0, -0.8, maxVal + 0.5, barCount);
-            }
-        } else {
-            for (BarStruct value: trianglesInRenderer.values()) {
-                if (value.tri.getBounds().getMaxY() > maxVal)
-                    maxVal = (int) Math.ceil(value.tri.getBounds().getMaxY());
-                if (value.tri.getBounds().getMinY() < minVal)
-                    minVal = (int) Math.ceil(value.tri.getBounds().getMinY());
-            }
-            if (minVal < 0) {
-                this.coordsys.setCoordinateView(-0.8, minVal - 0.5, barCount, maxVal + 0.5);
-            } else {
-                this.coordsys.setCoordinateView(-0.8, 0, barCount, maxVal + 0.5);
-            }
+    public BarChart setBarContentBoundaries() {
+        double maxVal = Integer.MIN_VALUE; double minVal = Integer.MAX_VALUE;
+        for (BarStruct value: trianglesInRenderer.values()) {
+            maxVal = Math.max(value.barLength, maxVal);
+            minVal = Math.min(value.barLength, minVal);
         }
+        if (minVal >= 0) {
+            minVal = 0.5;
+        }
+        if (this.chartAlignment == AlignmentConstants.HORIZONTAL) {
+            this.barRenderer.setCoordinateView(minVal - 0.5, -0.8, maxVal + 0.5, barCount);
+        } else if (this.chartAlignment == AlignmentConstants.VERTICAL) {
+            this.barRenderer.setCoordinateView(-0.8, minVal - 0.5, barCount, maxVal + 0.5);
+        }
+        return this;
     }
 
     // orders by height
-    public void orderBars() {
-        for (BarStruct val: trianglesInRenderer.values()) {
-            this.content.triangles.removeItemToRender(val.tri);
-        }
-        AtomicInteger index = new AtomicInteger();
-        if (this.chartAlignment == Alignment.HORIZONTAL) {
-            trianglesInRenderer.values().stream()
-                    .sorted(Comparator.comparingDouble(e -> e.tri.getBounds().getMinX()))
-                    .sorted(Comparator.comparingDouble(e -> e.tri.getBounds().getMaxX()))
-                    .forEach(e -> {
-                        Triangles tri;
-                        if (e.tri.getBounds().getMaxX() == 0)
-                            tri = makeBar(index.getAndIncrement(), e.tri.getBounds().getMinX(), new Color(e.tri.getTriangleDetails().get(0).c0.getAsInt()));
-                        else
-                            tri = makeBar(index.getAndIncrement(), e.tri.getBounds().getMaxX(), new Color(e.tri.getTriangleDetails().get(0).c0.getAsInt()));
-                        this.content.addItemToRender(tri);
-                    });
-        } else {
-            trianglesInRenderer.values().stream()
-                    .sorted(Comparator.comparingDouble(e -> e.tri.getBounds().getMinY()))
-                    .sorted(Comparator.comparingDouble(e -> e.tri.getBounds().getMaxY()))
-                    .forEach(e -> {
-                        Triangles tri;
-                        if (e.tri.getBounds().getMaxY() == 0)
-                            tri = makeBar(index.getAndIncrement(), e.tri.getBounds().getMinY(), new Color(e.tri.getTriangleDetails().get(0).c0.getAsInt()));
-                        else
-                            tri = makeBar(index.getAndIncrement(), e.tri.getBounds().getMaxY(), new Color(e.tri.getTriangleDetails().get(0).c0.getAsInt()));
-                        this.content.addItemToRender(tri);
-            });
-        }
+    public BarChart sortBars() {
+        sortBars((o1, o2) -> {
+            if (o1 < o2) return -1;
+            if (o1 > o2) return 1;
+            return 0;
+        });
+        return this;
     }
+
+    public BarChart sortBars(final Comparator<Double> comparator) {
+        removeAllBars();
+        AtomicInteger index = new AtomicInteger();
+        trianglesInRenderer.values().stream()
+                .sorted((a, b) -> comparator.compare(a.barLength, b.barLength))
+                .forEach(e -> reconstructBars(index, e));
+        setTickmarks();
+        return this;
+    }
+
+    public BarChart sortBarsIDs() {
+        removeAllBars();
+        AtomicInteger index = new AtomicInteger();
+        trianglesInRenderer.values().forEach(e -> reconstructBars(index, e));
+        setTickmarks();
+        return this;
+    }
+
+    protected BarChart reconstructBars(AtomicInteger index, BarStruct struct) {
+        double missingVals = struct.barLength;
+        struct.position = index.incrementAndGet();
+        for (Triangles element: struct.stacks) {
+            double length = 0;
+            if (chartAlignment == AlignmentConstants.HORIZONTAL) {
+                if (element.getBounds().getMaxX() == 0)
+                    length = element.getBounds().getMinX();
+                else
+                    length = element.getBounds().getMaxX();
+            } else if (chartAlignment == AlignmentConstants.VERTICAL) {
+                if (element.getBounds().getMaxY() == 0)
+                    length = element.getBounds().getMinY();
+                else
+                    length = element.getBounds().getMaxY();
+            }
+            Triangles tri = makeBar((int) (struct.barLength - missingVals), struct.position - 1, length, new Color(element.getTriangleDetails().get(0).c0.getAsInt()));
+            missingVals -= length;
+            this.content.addItemToRender(tri);
+        }
+        return this;
+    }
+
+    public BarChart removeAllBars() {
+        LinkedList<Triangles> items = new LinkedList<>(this.content.triangles.getItemsToRender());
+        for (Triangles tri : items) {
+            this.content.triangles.removeItemToRender(tri);
+        }
+        return this;
+    }
+
+    public BarChart changeRotation(final int chartAlignment) {
+        this.chartAlignment = chartAlignment;
+        return this;
+    }
+
+    // add stacks
 
     // TODO discuss interaction methods - when ScatterPlot is finished
     /*    - panning
@@ -224,17 +259,11 @@ public class BarChart {
           -
     */
 
+    // TODO zwei y bzw. x Achsen für Beschreibungen
     // TODO bars stacking on top of each other
+    // TODO gruppierungen - idea: https://kb.tableau.com/articles/howto/creation-of-a-grouped-bar-chart
 
     // TODO text wraparound, when too long
-
-
-    // TODO necessary?
-    public BarChart changeRotation(final Alignment chartAlignment) {
-        this.chartAlignment = chartAlignment;
-
-        return this;
-    }
 
     public CompleteRenderer getContent() {
         return content;
@@ -244,11 +273,11 @@ public class BarChart {
         return canvas;
     }
 
-    public CoordSysRenderer getCoordsys() {
-        return coordsys;
+    public BarRenderer getBarRenderer() {
+        return barRenderer;
     }
 
-    public Alignment getChartAlignment() {
+    public int getChartAlignment() {
         return chartAlignment;
     }
 
