a3f4dea7120f8367273ab3b1935f7c8e7b40eff1
Luca Reichmann <luca@lucareichmann.de>
2021-06-23 14:11:25 +0200

    added convenience method for adding legend

--- jplotter/src/main/java/hageldave/jplotter/charts/CombinedBarChart.java
+++ jplotter/src/main/java/hageldave/jplotter/charts/CombinedBarChart.java
@@ -4,17 +4,19 @@ import hageldave.jplotter.canvas.BlankCanvas;
 import hageldave.jplotter.canvas.BlankCanvasFallback;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 import hageldave.jplotter.renderables.BarGroup;
+import hageldave.jplotter.renderables.Legend;
 import hageldave.jplotter.renderers.CombinedBarRenderer;
 import hageldave.jplotter.renderers.TrianglesRenderer;
 import hageldave.jplotter.util.AlignmentConstants;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 public class CombinedBarChart {
-
     protected TrianglesRenderer content;
     protected JPlotterCanvas canvas;
     protected CombinedBarRenderer barRenderer;
+    protected LinkedList<BarGroup> barsInRenderer = new LinkedList<>();
 
     public CombinedBarChart(final boolean useOpenGL) {
         this(useOpenGL ? new BlankCanvas() : new BlankCanvasFallback(), "X", "Y");
@@ -24,7 +26,7 @@ public class CombinedBarChart {
         this.canvas = canvas;
         this.canvas.asComponent().setPreferredSize(new Dimension(400, 400));
         this.canvas.asComponent().setBackground(Color.WHITE);
-        this.barRenderer = new CombinedBarRenderer(AlignmentConstants.VERTICAL);
+        this.barRenderer = new CombinedBarRenderer(AlignmentConstants.HORIZONTAL);
         this.content = new TrianglesRenderer();
         this.barRenderer.setCoordinateView(-1, -1, 1, 1);
         this.barRenderer.setContent(content);
@@ -34,8 +36,45 @@ public class CombinedBarChart {
     }
 
     public CombinedBarChart addData(BarGroup group) {
+        this.barsInRenderer.add(group);
         this.barRenderer.addBarGroup(group);
         return this;
+    }
+
+    public Legend addRightLegend(final int width, final boolean autoAddItems) {
+        Legend lgd = new Legend();
+        this.barRenderer.setLegendRight(lgd);
+        this.barRenderer.setLegendRightWidth(width);
+        for (BarGroup group: barsInRenderer) {
+            for (BarGroup.BarStruct struct: group.getGroupedBars().values()) {
+                for (BarGroup.Stack stack: struct.stacks) {
+                    lgd.addBarLabel(stack.stackColor.getRGB(), struct.description, stack.pickColor);
+                }
+            }
+        }
+        return lgd;
+    }
+
+    public Legend addRightLegend() {
+        return addRightLegend(70, false);
+    }
+
+    public Legend addBottomLegend(final int height, final boolean autoAddItems) {
+        Legend lgd = new Legend();
+        this.barRenderer.setLegendBottom(lgd);
+        this.barRenderer.setLegendBottomHeight(height);
+        for (BarGroup group: barsInRenderer) {
+            for (BarGroup.BarStruct struct: group.getGroupedBars().values()) {
+                for (BarGroup.Stack stack: struct.stacks) {
+                    lgd.addBarLabel(stack.stackColor.getRGB(), struct.description, stack.pickColor);
+                }
+            }
+        }
+        return lgd;
+    }
+
+    public Legend addBottomLegend() {
+        return addBottomLegend(30, false);
     }
 
     public TrianglesRenderer getContent() {
