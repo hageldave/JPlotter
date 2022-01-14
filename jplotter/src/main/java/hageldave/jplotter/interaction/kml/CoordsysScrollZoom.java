25f8e60350b4e888fe2567bada8661440ae5d0bc
Luca Reichmann <luca@lucareichmann.de>
2022-01-14 18:13:36 +0100

    renamed CoordSysScrollZoom

--- jplotter/src/main/java/hageldave/jplotter/interaction/kml/CoordsysScrollZoom.java
+++ jplotter/src/main/java/hageldave/jplotter/interaction/kml/CoordsysScrollZoom.java
@@ -25,23 +25,27 @@ import java.util.Arrays;
  * @author hageldave
  *
  */
-// TODO: better name, maybe FocusedCoordsysScrollZoom
-public class DynamicCoordsysScrollZoom implements MouseWheelListener, InteractionConstants {
-
+public class CoordsysScrollZoom implements MouseWheelListener, InteractionConstants {
     protected Component canvas;
     protected CoordSysRenderer coordsys;
     protected double zoomFactor = 1.7;
     protected int axes = X_AXIS | Y_AXIS;
     protected KeyMaskListener keyListenerMask;
+    protected boolean mouseFocusedZoom;
 
-    public DynamicCoordsysScrollZoom(JPlotterCanvas canvas, CoordSysRenderer coordsys, KeyMaskListener keyListenerMask) {
+    public CoordsysScrollZoom(JPlotterCanvas canvas, CoordSysRenderer coordsys, KeyMaskListener keyListenerMask, boolean mouseFocusedZoom) {
         this.canvas = canvas.asComponent();
         this.coordsys = coordsys;
         this.keyListenerMask = keyListenerMask;
+        this.mouseFocusedZoom = mouseFocusedZoom;
     }
 
-    public DynamicCoordsysScrollZoom(JPlotterCanvas canvas, CoordSysRenderer coordsys) {
-        this(canvas, coordsys, new KeyMaskListener(KeyEvent.VK_ALT));
+    public CoordsysScrollZoom(JPlotterCanvas canvas, CoordSysRenderer coordsys, boolean mouseFocusedZoom) {
+        this(canvas, coordsys, new KeyMaskListener(KeyEvent.VK_ALT), mouseFocusedZoom);
+    }
+
+    public CoordsysScrollZoom(JPlotterCanvas canvas, CoordSysRenderer coordsys) {
+        this(canvas, coordsys, new KeyMaskListener(KeyEvent.VK_ALT), false);
     }
 
     @Override
@@ -52,40 +56,58 @@ public class DynamicCoordsysScrollZoom implements MouseWheelListener, Interactio
 
             double coordsysPosX = coordsys.transformAWT2CoordSys(e.getPoint(), canvas.getHeight()).getX();
             double coordsysPosY = coordsys.transformAWT2CoordSys(e.getPoint(), canvas.getHeight()).getY();
-
             int wheelRotation = e.getWheelRotation();
-            double zoom = Math.pow(zoomFactor, wheelRotation*0.1);
+            double zoom = Math.pow(zoomFactor, wheelRotation*0.7);
 
-            AffineTransform at = new AffineTransform();
-            at.translate(coordsysPosX, coordsysPosY);
-            at.scale(zoom, zoom);
-            at.translate(-coordsysPosX, -coordsysPosY);
+            // zoom relative to mouse position
+            if (mouseFocusedZoom) {
+                AffineTransform at = new AffineTransform();
+                at.translate(coordsysPosX, coordsysPosY);
+                at.scale(zoom, zoom);
+                at.translate(-coordsysPosX, -coordsysPosY);
 
-            Rectangle2D.Double r2d = new Rectangle2D.Double(
-                    coordsys.getCoordinateView().getX(),
-                    coordsys.getCoordinateView().getY(),
-                    coordsys.getCoordinateView().getMaxX()-coordsys.getCoordinateView().getX(),
-                    coordsys.getCoordinateView().getMaxY()-coordsys.getCoordinateView().getY());
-            Shape transformedR2D = at.createTransformedShape(r2d);
+                Rectangle2D.Double r2d = new Rectangle2D.Double(
+                        coordsys.getCoordinateView().getX(),
+                        coordsys.getCoordinateView().getY(),
+                        coordsys.getCoordinateView().getMaxX() - coordsys.getCoordinateView().getX(),
+                        coordsys.getCoordinateView().getMaxY() - coordsys.getCoordinateView().getY());
+                Shape transformedR2D = at.createTransformedShape(r2d);
 
-            coordsys.setCoordinateView(
-                    transformedR2D.getBounds2D().getX(),
-                    transformedR2D.getBounds2D().getY(),
-                    transformedR2D.getBounds2D().getMaxX(),
-                    transformedR2D.getBounds2D().getMaxY()
-            );
+                coordsys.setCoordinateView(
+                        transformedR2D.getBounds2D().getX(),
+                        transformedR2D.getBounds2D().getY(),
+                        transformedR2D.getBounds2D().getMaxX(),
+                        transformedR2D.getBounds2D().getMaxY()
+                );
+            // zoom into the center of the coordsys
+            } else {
+                double centerX = coordsys.getCoordinateView().getCenterX();
+                double centerY = coordsys.getCoordinateView().getCenterY();
+                double width = coordsys.getCoordinateView().getWidth();
+                double height = coordsys.getCoordinateView().getHeight();
+                if (( axes & X_AXIS ) != 0)
+                    width *= zoom;
+                if (( axes & Y_AXIS ) != 0)
+                    height *= zoom;
+                coordsys.setCoordinateView(
+                        centerX - width / 2,
+                        centerY - height / 2,
+                        centerX + width / 2,
+                        centerY + height / 2
+                );
+            }
             canvas.repaint();
         }
     }
 
     /**
-     * Sets the zoom factor of this {@link CoordSysScrollZoom}.
+     * Sets the zoom factor of this {@link CoordsysScrollZoom}.
      * The default value is 2.0.
      * Using a value in ]0,1[ will reverse the zoom direction.
      * @param zoomFactor to be set
      * @return this for chaining
      */
-    public DynamicCoordsysScrollZoom setZoomFactor(double zoomFactor) {
+    public CoordsysScrollZoom setZoomFactor(double zoomFactor) {
         this.zoomFactor = zoomFactor;
         return this;
     }
@@ -95,10 +117,10 @@ public class DynamicCoordsysScrollZoom implements MouseWheelListener, Interactio
     }
 
     /**
-     * Adds this {@link CoordSysScrollZoom} as {@link MouseWheelListener} to the associated canvas.
+     * Adds this {@link CoordsysScrollZoom} as {@link MouseWheelListener} to the associated canvas.
      * @return this for chaining
      */
-    public DynamicCoordsysScrollZoom register(){
+    public CoordsysScrollZoom register(){
         if( ! Arrays.asList(canvas.getMouseWheelListeners()).contains(this))
             canvas.addMouseWheelListener(this);
         if (!Arrays.asList(canvas.getKeyListeners()).contains(this.keyListenerMask))
@@ -112,7 +134,7 @@ public class DynamicCoordsysScrollZoom implements MouseWheelListener, Interactio
      * @param axes {@link InteractionConstants#X_AXIS}, {@link InteractionConstants#Y_AXIS} or {@code X_AXIS|Y_AXIS}
      * @return this for chaining
      */
-    public DynamicCoordsysScrollZoom setZoomedAxes(int axes){
+    public CoordsysScrollZoom setZoomedAxes(int axes){
         this.axes = axes;
         return this;
     }
@@ -125,6 +147,15 @@ public class DynamicCoordsysScrollZoom implements MouseWheelListener, Interactio
         return axes;
     }
 
+    public boolean isMouseFocusedZoom() {
+        return mouseFocusedZoom;
+    }
+
+    public CoordsysScrollZoom setMouseFocusedZoom(boolean mouseFocusedZoom) {
+        this.mouseFocusedZoom = mouseFocusedZoom;
+        return this;
+    }
+
     public void setKeyListenerMask(KeyMaskListener keyListenerMask) {
         canvas.removeKeyListener(this.keyListenerMask);
         this.keyListenerMask = keyListenerMask;
@@ -133,11 +164,11 @@ public class DynamicCoordsysScrollZoom implements MouseWheelListener, Interactio
     }
 
     /**
-     * Removes this {@link CoordSysScrollZoom} from the associated canvas'
+     * Removes this {@link CoordsysScrollZoom} from the associated canvas'
      * mouse wheel listeners.
      * @return this for chaining
      */
-    public DynamicCoordsysScrollZoom deRegister(){
+    public CoordsysScrollZoom deRegister(){
         canvas.removeMouseWheelListener(this);
         canvas.removeKeyListener(this.keyListenerMask);
         return this;
