78d1624d3e96ded904d11c180b3759b7279e0bba
Luca Reichmann <luca@lucareichmann.de>
2022-05-31 17:45:33 +0200

    improved textfield label

--- jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/TextfieldCreator.java
+++ jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/TextfieldCreator.java
@@ -3,30 +3,36 @@ package hageldave.jplotter.debugging.controlHandler.panelcreators.control;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TextfieldCreator implements ControlPanelCreator {
     @Override
     public JPanel create(JPlotterCanvas canvas, Object obj, JPanel labelContainer, Method setter, Method getter) throws Exception {
-        JLabel fieldValLabel = new JLabel(String.valueOf(getter.invoke(obj)));
+        JLabel textLabel = new JLabel("Current string: " + getter.invoke(obj));
+        JTextField textField = new JTextField("", 20);
+        textField.setMaximumSize(textField.getPreferredSize());
 
-        JTextField textfield = new JTextField("", 20);
-        textfield.setMaximumSize(textfield.getPreferredSize());
-
-        textfield.addActionListener(e -> {
+        textField.addActionListener(e -> {
             try {
-                setter.invoke(obj, textfield.getText());
-                fieldValLabel.setText((String) getter.invoke(obj));
-                textfield.setText("");
+                setter.invoke(obj, textField.getText());
+                textLabel.setText("Current string: " + getter.invoke(obj));
+                textField.setText("");
             } catch (IllegalAccessException | InvocationTargetException ex) {
                 ex.printStackTrace();
             }
             canvas.scheduleRepaint();
         });
 
-        labelContainer.add(fieldValLabel);
-        labelContainer.add(textfield);
+        labelContainer.setLayout(new BoxLayout(labelContainer, BoxLayout.Y_AXIS));
+        labelContainer.setBorder(new EmptyBorder(10, 0, 7, 0));
+        textLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        textField.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        labelContainer.add(textLabel);
+        labelContainer.add(textField);
         return labelContainer;
     }
 }
