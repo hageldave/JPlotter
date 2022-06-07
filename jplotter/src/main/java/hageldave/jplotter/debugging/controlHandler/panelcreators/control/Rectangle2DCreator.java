b5ac1a1bc659e13364530b8e7ab8b9640622b567
Luca Reichmann <luca@lucareichmann.de>
2022-06-07 13:18:27 +0200

    added labels to Rectangle2DCreator's input fields

--- jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/Rectangle2DCreator.java
+++ jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/Rectangle2DCreator.java
@@ -3,70 +3,25 @@ package hageldave.jplotter.debugging.controlHandler.panelcreators.control;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Rectangle2DCreator implements ControlPanelCreator {
+    protected SpinnerNumberModel xMinCoordModel;
+    protected SpinnerNumberModel xMaxCoordModel;
+    protected SpinnerNumberModel yMinCoordModel;
+    protected SpinnerNumberModel yMaxCoordModel;
+
     @Override
     public JPanel create(JPlotterCanvas canvas, Object obj, JPanel labelContainer, Method setter, Method getter) throws Exception {
         Rectangle2D initValue = (Rectangle2D) getter.invoke(obj);
 
-        SpinnerNumberModel xMinCoordModel = new SpinnerNumberModel((int) initValue.getMinX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
-        JSpinner xMinCoord = new JSpinner(xMinCoordModel);
-        xMinCoord.setMaximumSize(xMinCoord.getMinimumSize());
-
-        SpinnerNumberModel xMaxCoordModel = new SpinnerNumberModel((int) initValue.getMaxX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
-        JSpinner xMaxCoord = new JSpinner(xMaxCoordModel);
-        xMaxCoord.setMaximumSize(xMaxCoord.getMinimumSize());
-
-        SpinnerNumberModel yMinCoordModel = new SpinnerNumberModel((int) initValue.getMinY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
-        JSpinner yMinCoord = new JSpinner(yMinCoordModel);
-        yMinCoord.setMaximumSize(yMinCoord.getMinimumSize());
-
-        SpinnerNumberModel yMaxCoordModel = new SpinnerNumberModel((int) initValue.getMaxY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
-        JSpinner yMaxCoord = new JSpinner(yMaxCoordModel);
-        yMaxCoord.setMaximumSize(yMaxCoord.getMinimumSize());
-
-        xMinCoord.addChangeListener(e -> addListener(
-                (double) xMinCoord.getModel().getValue(),
-                (double) xMaxCoord.getModel().getValue(),
-                (double) yMinCoord.getModel().getValue(),
-                (double) yMaxCoord.getModel().getValue(),
-                setter,
-                obj,
-                canvas)
-        );
-
-        yMinCoord.addChangeListener(e -> addListener(
-                (double) xMinCoord.getModel().getValue(),
-                (double) xMaxCoord.getModel().getValue(),
-                (double) yMinCoord.getModel().getValue(),
-                (double) yMaxCoord.getModel().getValue(),
-                setter,
-                obj,
-                canvas)
-        );
-
-        xMaxCoord.addChangeListener(e -> addListener(
-                (double) xMinCoord.getModel().getValue(),
-                (double) xMaxCoord.getModel().getValue(),
-                (double) yMinCoord.getModel().getValue(),
-                (double) yMaxCoord.getModel().getValue(),
-                setter,
-                obj,
-                canvas)
-        );
-
-        yMaxCoord.addChangeListener(e -> addListener(
-                (double) xMinCoord.getModel().getValue(),
-                (double) xMaxCoord.getModel().getValue(),
-                (double) yMinCoord.getModel().getValue(),
-                (double) yMaxCoord.getModel().getValue(),
-                setter,
-                obj,
-                canvas)
-        );
+        this.xMinCoordModel = new SpinnerNumberModel(initValue.getMinX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
+        this.xMaxCoordModel = new SpinnerNumberModel(initValue.getMaxX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
+        this.yMinCoordModel = new SpinnerNumberModel(initValue.getMinY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
+        this.yMaxCoordModel = new SpinnerNumberModel(initValue.getMaxY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 0.1);
 
         JPanel topContainer = new JPanel();
         JPanel bottomContainer = new JPanel();
@@ -75,10 +30,10 @@ public class Rectangle2DCreator implements ControlPanelCreator {
         topContainer.setLayout(new BoxLayout(topContainer, BoxLayout.X_AXIS));
         bottomContainer.setLayout(new BoxLayout(bottomContainer, BoxLayout.X_AXIS));
 
-        topContainer.add(xMinCoord);
-        topContainer.add(xMaxCoord);
-        bottomContainer.add(yMinCoord);
-        bottomContainer.add(yMaxCoord);
+        topContainer.add(constructCoordContainer(xMinCoordModel, "X Min", setter, obj, canvas));
+        topContainer.add(constructCoordContainer(xMaxCoordModel, "X Max", setter, obj, canvas));
+        bottomContainer.add(constructCoordContainer(yMinCoordModel, "Y Min", setter, obj, canvas));
+        bottomContainer.add(constructCoordContainer(yMaxCoordModel, "Y Max", setter, obj, canvas));
 
         labelContainer.add(topContainer);
         labelContainer.add(bottomContainer);
@@ -92,6 +47,32 @@ public class Rectangle2DCreator implements ControlPanelCreator {
             throw new RuntimeException(ex);
         }
         canvas.scheduleRepaint();
+    }
+
+    private JPanel constructCoordContainer(SpinnerNumberModel spinnerNumberModel, String label, Method setter, Object obj, JPlotterCanvas canvas) {
+        JPanel coordContainer = new JPanel();
+        coordContainer.setLayout(new BoxLayout(coordContainer, BoxLayout.Y_AXIS));
+        JLabel coordLabel = new JLabel(label);
+
+        JSpinner coordSpinner = new JSpinner(spinnerNumberModel);
+        coordSpinner.setMaximumSize(coordSpinner.getMinimumSize());
+
+        coordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        coordSpinner.setAlignmentX(Component.LEFT_ALIGNMENT);
+        coordContainer.add(coordLabel);
+        coordContainer.add(coordSpinner);
+
+        coordSpinner.addChangeListener(e -> addListener(
+                (double) this.xMinCoordModel.getValue(),
+                (double) this.yMinCoordModel.getValue(),
+                (double) this.xMaxCoordModel.getValue(),
+                (double) this.yMaxCoordModel.getValue(),
+                setter,
+                obj,
+                canvas)
+        );
+
+        return coordContainer;
     }
 }
 
