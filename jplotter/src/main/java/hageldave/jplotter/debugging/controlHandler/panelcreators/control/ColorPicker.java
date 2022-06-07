911da794494caba0c5ce36a64f6c5ba1e8e0565e
Luca Reichmann <luca@lucareichmann.de>
2022-06-07 14:49:41 +0200

    improved color picker layout & jColorPicker now remembers last chosen color

--- jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/ColorPicker.java
+++ jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/ColorPicker.java
@@ -3,36 +3,49 @@ package hageldave.jplotter.debugging.controlHandler.panelcreators.control;
 import hageldave.jplotter.canvas.JPlotterCanvas;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ColorPicker implements ControlPanelCreator {
     @Override
     public JPanel create(JPlotterCanvas canvas, Object obj, JPanel labelContainer, Method setter, Method getter) throws Exception {
-        Color currentColor = (Color) getter.invoke(obj);
+        AtomicReference<Color> currentColor = new AtomicReference<>((Color) getter.invoke(obj));
+
+        labelContainer.setLayout(new BoxLayout(labelContainer, BoxLayout.Y_AXIS));
+        labelContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        JPanel colorInfoContainer = new JPanel();
+        colorInfoContainer.setLayout(new BoxLayout(colorInfoContainer, BoxLayout.X_AXIS));
+        colorInfoContainer.setBorder(new EmptyBorder(10, 0, 7, 0));
+        colorInfoContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         JLabel rgbLabel = new JLabel("RBG value: ");
-        JLabel redValue = new JLabel(String.valueOf(currentColor.getRed()));
+
+        JLabel redValue = new JLabel(String.valueOf(currentColor.get().getRed()));
         redValue.setToolTipText("Value of the red color component.");
 
-        JLabel greenValue = new JLabel(", " + currentColor.getGreen());
+        JLabel greenValue = new JLabel(", " + currentColor.get().getGreen());
         greenValue.setToolTipText("Value of the green color component.");
 
-        JLabel blueValue = new JLabel(", " + currentColor.getBlue());
+        JLabel blueValue = new JLabel(", " + currentColor.get().getBlue());
         blueValue.setToolTipText("Value of the blue color component.");
 
-        JLabel alphaValue = new JLabel(", " + currentColor.getAlpha());
+        JLabel alphaValue = new JLabel(", " + currentColor.get().getAlpha());
         alphaValue.setToolTipText("Value of the alpha color component.");
 
         JButton editButton = new JButton("Edit color");
+        editButton.setMargin(new Insets(0,0,0,0));
+        editButton.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        Color textColor = new Color(currentColor.getRed(), currentColor.getGreen(), currentColor.getBlue());
+        Color textColor = new Color(currentColor.get().getRed(), currentColor.get().getGreen(), currentColor.get().getBlue());
         ColoredRectangle coloredRectangle = new ColoredRectangle(textColor);
 
         editButton.addActionListener(e -> {
-            Color selectedColor = JColorChooser.showDialog(labelContainer, "Pick a color", currentColor);
+            Color selectedColor = JColorChooser.showDialog(labelContainer, "Pick a color", currentColor.get());
             try {
                 if (Objects.nonNull(selectedColor)) {
                     setter.invoke(obj, selectedColor);
@@ -41,6 +54,7 @@ public class ColorPicker implements ControlPanelCreator {
                     blueValue.setText(", " + selectedColor.getBlue());
                     alphaValue.setText(", " + selectedColor.getAlpha());
                     coloredRectangle.updateColor(new Color(selectedColor.getRGB(), true));
+                    currentColor.set(selectedColor);
                 }
             } catch (IllegalAccessException | InvocationTargetException ex) {
                 throw new RuntimeException(ex);
@@ -48,19 +62,20 @@ public class ColorPicker implements ControlPanelCreator {
             canvas.scheduleRepaint();
         });
 
-        labelContainer.add(coloredRectangle);
-        labelContainer.add(rgbLabel);
-        labelContainer.add(redValue);
-        labelContainer.add(greenValue);
-        labelContainer.add(blueValue);
-        labelContainer.add(alphaValue);
+        colorInfoContainer.add(coloredRectangle);
+        colorInfoContainer.add(rgbLabel);
+        colorInfoContainer.add(redValue);
+        colorInfoContainer.add(greenValue);
+        colorInfoContainer.add(blueValue);
+        colorInfoContainer.add(alphaValue);
+        labelContainer.add(colorInfoContainer);
         labelContainer.add(editButton);
         return labelContainer;
     }
 
     private static class ColoredRectangle extends JComponent {
         protected Color c;
-        protected int size = 20;
+        protected int size = 17;
         public ColoredRectangle(Color c) {
             this.c = c;
             this.setMaximumSize(new Dimension(size + 5, size));
