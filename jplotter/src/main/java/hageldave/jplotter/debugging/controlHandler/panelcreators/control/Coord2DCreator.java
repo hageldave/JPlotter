f55410fd2f49551baf3ad8d2518d1e5992796de2
Luca Reichmann <luca@lucareichmann.de>
2022-06-07 13:48:00 +0200

    refactoring of Coord2DCreator

--- jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/Coord2DCreator.java
+++ jplotter/src/main/java/hageldave/jplotter/debugging/controlHandler/panelcreators/control/Coord2DCreator.java
@@ -10,59 +10,50 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Coord2DCreator implements ControlPanelCreator {
+    protected SpinnerNumberModel xCoordModel;
+    protected SpinnerNumberModel yCoordModel;
+
     @Override
     public JPanel create(JPlotterCanvas canvas, Object obj, JPanel labelContainer, Method setter, Method getter) throws Exception {
         Point2D initValue = (Point2D) getter.invoke(obj);
-
         labelContainer.setBorder(new EmptyBorder(10, 0, 7, 0));
-
-        JLabel xCoordLabel = new JLabel("X Coordinate");
-        SpinnerNumberModel xCoordModel = new SpinnerNumberModel((int) initValue.getX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
-        JSpinner xCoord = new JSpinner(xCoordModel);
-        xCoord.setMaximumSize(xCoord.getMinimumSize());
-
-        JPanel xCoordContainer = new JPanel();
-        xCoordContainer.setLayout(new BoxLayout(xCoordContainer, BoxLayout.Y_AXIS));
-        xCoordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        xCoord.setAlignmentX(Component.LEFT_ALIGNMENT);
-        xCoordContainer.add(xCoordLabel);
-        xCoordContainer.add(xCoord);
-
-        JLabel yCoordLabel = new JLabel("Y Coordinate");
-        SpinnerNumberModel yCoordModel = new SpinnerNumberModel((int) initValue.getY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
-        JSpinner yCoord = new JSpinner(yCoordModel);
-        yCoord.setMaximumSize(yCoord.getMinimumSize());
-
-        JPanel yCoordContainer = new JPanel();
-        yCoordContainer.setLayout(new BoxLayout(yCoordContainer, BoxLayout.Y_AXIS));
-        yCoordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        yCoord.setAlignmentX(Component.LEFT_ALIGNMENT);
-        yCoordContainer.add(yCoordLabel);
-        yCoordContainer.add(yCoord);
-
-        xCoord.addChangeListener(e -> {
-            try {
-                int xCoordValue = (int) xCoord.getModel().getValue();
-                int yCoordValue = (int) yCoord.getModel().getValue();
-                setter.invoke(obj, xCoordValue, yCoordValue);
-            } catch (IllegalAccessException | InvocationTargetException ex) {
-                throw new RuntimeException(ex);
-            }
-            canvas.scheduleRepaint();
-        });
-
-        yCoord.addChangeListener(e -> {
-            try {
-                int xCoordValue = (int) xCoord.getModel().getValue();
-                int yCoordValue = (int) yCoord.getModel().getValue();
-                setter.invoke(obj, xCoordValue, yCoordValue);
-            } catch (IllegalAccessException | InvocationTargetException ex) {
-                throw new RuntimeException(ex);
-            }
-            canvas.scheduleRepaint();
-        });
-        labelContainer.add(xCoordContainer);
-        labelContainer.add(yCoordContainer);
+        this.xCoordModel = new SpinnerNumberModel((int) initValue.getX(), Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
+        this.yCoordModel = new SpinnerNumberModel((int) initValue.getY(), Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
+        labelContainer.add(constructCoordContainer(xCoordModel, "X Coordinate", setter, obj, canvas));
+        labelContainer.add(constructCoordContainer(yCoordModel, "Y Coordinate", setter, obj, canvas));
         return labelContainer;
+    }
+
+    private void addListener(int xCoordValue, int yCoordValue, Method setter, Object obj, JPlotterCanvas canvas) {
+        try {
+            setter.invoke(obj, xCoordValue, yCoordValue);
+        } catch (IllegalAccessException | InvocationTargetException ex) {
+            throw new RuntimeException(ex);
+        }
+        canvas.scheduleRepaint();
+    }
+
+    private JPanel constructCoordContainer(SpinnerNumberModel spinnerNumberModel, String label, Method setter, Object obj, JPlotterCanvas canvas) {
+        JPanel coordContainer = new JPanel();
+        coordContainer.setLayout(new BoxLayout(coordContainer, BoxLayout.Y_AXIS));
+        JLabel coordLabel = new JLabel(label);
+
+        JSpinner coordSpinner = new JSpinner(spinnerNumberModel);
+        coordSpinner.setMaximumSize(coordSpinner.getMinimumSize());
+
+        coordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        coordSpinner.setAlignmentX(Component.LEFT_ALIGNMENT);
+        coordContainer.add(coordLabel);
+        coordContainer.add(coordSpinner);
+
+        coordSpinner.addChangeListener(e -> addListener(
+                (int) this.xCoordModel.getValue(),
+                (int) this.yCoordModel.getValue(),
+                setter,
+                obj,
+                canvas)
+        );
+
+        return coordContainer;
     }
 }
