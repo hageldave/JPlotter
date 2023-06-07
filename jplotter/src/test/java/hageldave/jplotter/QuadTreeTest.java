f2bec387f6df22252d3f241e9b6aa0c8af60fb7e
Luca <52721603+lvcarx@users.noreply.github.com>
2023-06-07 15:22:11 +0200

    added some more testcases to QuadTreeTest class

--- jplotter/src/test/java/hageldave/jplotter/QuadTreeTest.java
+++ jplotter/src/test/java/hageldave/jplotter/QuadTreeTest.java
@@ -3,34 +3,57 @@ package hageldave.jplotter;
 import hageldave.jplotter.util.QuadTree;
 
 import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class QuadTreeTest {
+    public final static int MAX_CAPACITY = 4;
+
     public static void main(String[] args) {
         // Unit Tests
+        QuadTree<double[]> quadTree = new QuadTree<>(MAX_CAPACITY, new Rectangle2D.Double(0, 0, 100, 100), (double[] entry) -> entry[0], (double[] entry) -> entry[1]);
+        List<double[]> allPoints = insertPoints(quadTree);
+        testIfPointsAreReturned(quadTree, allPoints);
 
-        QuadTree<double[]> quadTree = new QuadTree<>(4, new Rectangle2D.Double(0, 0, 100, 100), (double[] entry) -> entry[0], (double[] entry) -> entry[1]);
+        // Test if the correct amount of subrectangles is created
+        testThatAllSubRectanglesAreCreated(quadTree);
 
+        // Test if the max capacity isn't too high in rects
+        testCapacity(quadTree);
+
+        // Print the tree
+        QuadTree.printTree(quadTree, Arrays::toString);
+    }
+
+    public static List<double[]> insertPoints(QuadTree<double[]> quadTree) {
         List<double[]> allPoints = new LinkedList<>();
+
+        // insert points to tree
         allPoints.add(new double[]{0, 0});
+        allPoints.add(new double[]{80, 45});
+        allPoints.add(new double[]{80, 80});
+        allPoints.add(new double[]{45, 80});
         allPoints.add(new double[]{20, 20});
         allPoints.add(new double[]{30, 20});
         allPoints.add(new double[]{30, 30});
         allPoints.add(new double[]{45, 30});
         allPoints.add(new double[]{30, 45});
-        allPoints.add(new double[]{30, 45});
         allPoints.add(new double[]{45, 45});
-        allPoints.add(new double[]{45, 45});
-        allPoints.add(new double[]{80, 45});
-        allPoints.add(new double[]{45, 80});
-        allPoints.add(new double[]{80, 80});
-//        allPoints.add(new double[]{100, 100});
+        allPoints.add(new double[]{100, 100});
 
         for (int i = 0; i < allPoints.size(); i++) {
             QuadTree.insert(quadTree, allPoints.get(i));
         }
+        return allPoints;
+    }
 
+    // Test if all points are found in the whole area
+    // Test if points are found in subareas
+    // Test edge cases with subareas / e.g. subareas that are overlapping
+    public static void testIfPointsAreReturned(QuadTree<double[]> quadTree, List<double[]> allPoints) {
         List<double[]> foundPointsInWholeArea = new LinkedList<>();
         QuadTree.getPointsInArea(foundPointsInWholeArea, quadTree, new Rectangle2D.Double(0, 0, 100, 100));
 
@@ -41,14 +64,14 @@ public class QuadTreeTest {
         List<double[]> foundPointsInSubarea = new LinkedList<>();
         QuadTree.getPointsInArea(foundPointsInSubarea, quadTree, new Rectangle2D.Double(25, 25, 25, 25));
 
-        if (foundPointsInSubarea.size() != 6) {
+        if (foundPointsInSubarea.size() != 4) {
             throw new RuntimeException();
         }
 
         foundPointsInSubarea = new LinkedList<>();
         QuadTree.getPointsInArea(foundPointsInSubarea, quadTree, new Rectangle2D.Double(0, 0, 50, 50));
 
-        if (foundPointsInSubarea.size() != 9) {
+        if (foundPointsInSubarea.size() != 7) {
             throw new RuntimeException();
         }
 
@@ -58,34 +81,25 @@ public class QuadTreeTest {
         if (foundPointsInSubarea.size() != 2) {
             throw new RuntimeException();
         }
+    }
 
-        // Test if all points are found in the whole area
-        // Test if points are found in subareas
-        // Test edge cases with subareas / e.g. subareas that are overlapping
+    public static void testThatAllSubRectanglesAreCreated(QuadTree<double[]> quadTree) {
+        if (Stream.of(quadTree.getLowerLeft(), quadTree.getUpperLeft(), quadTree.getLowerRight(), quadTree.getUpperRight()).anyMatch(Objects::isNull)) {
+            if (Stream.of(quadTree.getLowerLeft(), quadTree.getUpperLeft(), quadTree.getLowerRight(), quadTree.getUpperRight()).anyMatch(Objects::nonNull)) {
+                throw new RuntimeException();
+            }
+        }
+    }
 
-        // Test if the correct amount of subrectangles is created
-        // Test if the points are inserted at the correct level
-        // Test if the max capacity isn't too high in rects
-
-        // Level 1
-        QuadTree<double[]> level_1_LL = quadTree.getLowerLeft();
-        QuadTree<double[]> level_1_LR = quadTree.getLowerRight();
-        QuadTree<double[]> level_1_UL = quadTree.getUpperLeft();
-        QuadTree<double[]> level_1_UR = quadTree.getUpperRight();
-
-        // Level 2
-        QuadTree<double[]> level_2_LL = level_1_LL.getLowerLeft();
-        QuadTree<double[]> level_2_LR = level_1_LL.getLowerRight();
-        QuadTree<double[]> level_2_UL = level_1_LL.getUpperLeft();
-        QuadTree<double[]> level_2_UR = level_1_LL.getUpperRight();
-
-        // Level 3
-        QuadTree<double[]> level_3_LL = level_2_UR.getLowerLeft();
-        QuadTree<double[]> level_3_LR = level_2_UR.getLowerRight();
-        QuadTree<double[]> level_3_UL = level_2_UR.getUpperLeft();
-        QuadTree<double[]> level_3_UR = level_2_UR.getUpperRight();
-
-        // Test null checks
-
+    public static void testCapacity(QuadTree<double[]> quadTree) {
+        if (quadTree.getNodes().size() > MAX_CAPACITY) {
+            throw new RuntimeException();
+        }
+        if (quadTree.getLowerLeft() != null) {
+            testCapacity(quadTree.getLowerLeft());
+            testCapacity(quadTree.getLowerRight());
+            testCapacity(quadTree.getUpperLeft());
+            testCapacity(quadTree.getUpperRight());
+        }
     }
 }
